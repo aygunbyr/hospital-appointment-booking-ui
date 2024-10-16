0b31610ce8243e49ad82abf593e3e6f1d837bf51
Aygün Bayır <turkoaygun@gmail.com>
2024-10-16 15:32:54 +0300

    Updated Create Doctor Form and Create Patient Form

--- src/app/doctors/create/page.tsx
+++ src/app/doctors/create/page.tsx
@@ -1,88 +1,13 @@
-"use client";
-import { useState } from "react";
-import { useRouter } from "next/navigation";
 import Typography from "@mui/material/Typography";
-import Alert from "@mui/material/Alert";
-import DryForm from "@/components/DryForm";
-import { FormField } from "@/components/DryForm/types";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { createDoctor } from "@/services/apiService";
-import { Doctor } from "@/types/Doctor";
-import { CreateDoctorRequest } from "@/types/dtos/CreateDoctorRequest";
-import { ServiceResult } from "@/types/ServiceResult";
-import { AxiosError } from "axios";
+import CreateDoctorForm from "@/components/CreateDoctorForm";
 
 export default function CreateDoctor() {
-  const queryClient = useQueryClient();
-  const router = useRouter();
-  const [errors, setErrors] = useState<string[]>([]);
-
-  const formFields: FormField[] = [
-    {
-      label: "Name",
-      type: "text",
-      name: "name",
-      placeholder: "Name",
-      required: true,
-      validation: {
-        minLength: 2,
-        maxLength: 50,
-      },
-    },
-    {
-      label: "Branch",
-      type: "text",
-      name: "branch",
-      placeholder: "Branch",
-      required: true,
-      validation: {
-        minLength: 2,
-        maxLength: 30,
-      },
-    },
-  ];
-
-  const createDoctorMutation = useMutation<
-    ServiceResult<Doctor>,
-    unknown,
-    CreateDoctorRequest
-  >({
-    mutationFn: (createDoctorRequest: CreateDoctorRequest) =>
-      createDoctor(createDoctorRequest),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["doctors"] });
-      router.push("/doctors");
-    },
-    onError: (error) => {
-      // @ts-no-check
-      setErrors(
-        error instanceof AxiosError ? error.response?.data?.errorMessage : []
-      );
-    },
-  });
-
   return (
     <>
       <Typography variant="h4" mb={2}>
         Create Doctor
       </Typography>
-      {errors.map((error, index) => (
-        <Alert
-          key={index}
-          severity="error"
-          sx={{ width: "100%", marginBottom: 2 }}
-        >
-          {error}
-        </Alert>
-      ))}
-      <DryForm
-        mutationFn={{
-          mutate: createDoctorMutation.mutate,
-          error: createDoctorMutation.error,
-          data: createDoctorMutation.data,
-        }}
-        formFields={formFields}
-      />
+      <CreateDoctorForm />
     </>
   );
 }
--- src/app/patients/create/page.tsx
+++ src/app/patients/create/page.tsx
@@ -1,99 +1,13 @@
-"use client";
-import { useState } from "react";
-import { useRouter } from "next/navigation";
 import Typography from "@mui/material/Typography";
-import Alert from "@mui/material/Alert";
-import DryForm from "@/components/DryForm";
-import { FormField } from "@/components/DryForm/types";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { createPatient } from "@/services/apiService";
-import { Patient } from "@/types/Patient";
-import { CreatePatientRequest } from "@/types/dtos/CreatePatientRequest";
-import { ServiceResult } from "@/types/ServiceResult";
-import { AxiosError } from "axios";
+import CreatePatientForm from "@/components/CreatePatientForm";
 
-export default function CreatePatient() {
-  const queryClient = useQueryClient();
-  const router = useRouter();
-  const [errors, setErrors] = useState<string[]>([]);
-
-  const formFields: FormField[] = [
-    {
-      label: "Full Name",
-      type: "text",
-      name: "fullName",
-      placeholder: "Full Name",
-      required: true,
-      validation: {
-        minLength: 2,
-        maxLength: 50,
-      },
-    },
-    {
-      label: "Phone",
-      type: "text",
-      name: "phone",
-      placeholder: "Phone",
-      required: true,
-      validation: {
-        minLength: 2,
-        maxLength: 30,
-      },
-    },
-    {
-      label: "Citizen ID",
-      type: "text",
-      name: "citizenID",
-      placeholder: "Citizen ID",
-      required: true,
-      validation: {
-        minLength: 2,
-        maxLength: 30,
-      },
-    },
-  ];
-
-  const createPatientMutation = useMutation<
-    ServiceResult<Patient>,
-    unknown,
-    CreatePatientRequest
-  >({
-    mutationFn: (createPatientRequest: CreatePatientRequest) =>
-      createPatient(createPatientRequest),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["patients"] });
-      router.push("/patients");
-    },
-    onError: (error) => {
-      // @ts-no-check
-      setErrors(
-        error instanceof AxiosError ? error.response?.data?.errorMessage : []
-      );
-    },
-  });
-
+export default function CreateAppointment() {
   return (
     <>
       <Typography variant="h4" mb={2}>
         Create Patient
       </Typography>
-      {errors.map((error, index) => (
-        <Alert
-          key={index}
-          severity="error"
-          sx={{ width: "100%", marginBottom: 2 }}
-        >
-          {error}
-        </Alert>
-      ))}
-      <DryForm
-        mutationFn={{
-          mutate: createPatientMutation.mutate,
-          error: createPatientMutation.error,
-          data: createPatientMutation.data,
-        }}
-        formFields={formFields}
-      />
+      <CreatePatientForm />
     </>
   );
 }
